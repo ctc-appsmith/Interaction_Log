65c3b0aa8bc21ab93ddd3d93f41c48eb24e847b4
dluongo <dluongo@ctcnet.us>
2024-08-22 21:24:27 +0000

    Working through Sign-in functions.

--- pages/Home/jsobjects/auth_js/auth_js.js
+++ pages/Home/jsobjects/auth_js/auth_js.js
@@ -45,7 +45,49 @@ export default {
 			const [key, value] = i.split('=');
 			storeValue(key, value);
 		});
-	}
+	},
+	montanaURL: "https://ctcnetapps.us/app/grant-bid-tracker-mt/login-page-65bd14d46a813f5d1501a40d?branch=master",
+
+	
+
+  redirectToApp(redirect_url) {
+    const constructed_url = `${redirect_url}#access_token=${appsmith.store.access_token}&refresh_token=${appsmith.store.refresh_token}`;
+    window.location.href = constructed_url;
+  },
+
+  async signin_func(redirect_url) {
+    const api_call_name = "signin";
+		
+    const signin = async () => {
+      return await fetch('https://iuwrvlyhtutiqmjqrjbd.supabase.co/auth/v1/token?grant_type=password', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: appsmith.store.user.email,
+          password: appsmith.store.password,
+        }),
+      });
+    };
+		
+    await this.signin_func() // Calls the imported `signin` function
+      .then(data => {
+        Object.keys(data).forEach(i => {
+          storeValue(i, data[i]);
+        });
+
+        if (!appsmith.store.access_token) {
+          console.error("No access token found in authentication response");
+          showAlert("No access token found in authentication response", "error");
+        } else {
+          this.redirectToApp(redirect_url);
+        }
+      })
+      .catch(err => {
+        console.error("Error during signin:", err);
+      });
+  }
 }
 
 
