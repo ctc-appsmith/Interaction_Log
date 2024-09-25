5579c44203a3d67adcb3480b2e33050f8f23cd1a
dluongo <dluongo@ctcnet.us>
2024-09-25 16:20:40 +0000

    Creating JSObjects to run multiple queries on page load and main tab selection.

---
+++ pages/State Tracker/jsobjects/mainTabQueries/mainTabQueries.js
@@ -0,0 +1,58 @@
+export default {
+  // Function to run all the queries sequentially
+  runQueries: async () => {
+    try {
+      // Run getAppType
+      await getAppType.run();
+      
+      // Run getApplications
+      await getApplications.run();
+      
+      // Run getTeam
+      await getTeam.run();
+      
+      // Run getAppstatusType
+      await getappstatustype.run();
+      
+      // Run getEmployees
+      await getEmployees.run();
+      
+      // Run getTasks_byApplication_3
+      await getTasks_byApplication_3.run();
+      
+      // Run getTaskStatusType
+      await getTaskStatusType.run();
+      
+      // Run getTaskTypes_applicationtab
+      await getTaskTypes_applicationtab.run();
+      
+      // Run getApplicantsOnly
+      await getApplicantsOnly.run();
+      
+      // Run getMyTasks
+      await getMyTasks.run();
+			
+			// Run getApplications_byApplicant
+			await getApplications_byApplicant.run();
+			
+			// Run Rept_COUNT_openTask_TeamAppTyp
+			await Rept_COUNT_openTask_TeamAppTyp.run();
+			
+			// Run Rept_COUNT_unassignedTasks
+			await Rept_COUNT_unassignedTasks.run();
+			
+			// Run interactionLog_getEntityList
+			await interactionLog_getEntityList.run();
+			
+			// Run getEntityType
+			await getEntityType.run();
+			
+			// Run getApplicantStatus
+			await getApplicantStatus.run();
+
+      
+    } catch (error) {
+      console.error("Error running queries:", error);
+    }
+  }
+}
---
+++ pages/State Tracker/jsobjects/runningQueries_commentonly/runningQueries_commentonly.js
@@ -0,0 +1,65 @@
+
+
+//onAppLoad
+//	getAppType
+//	getApplications
+//	getTeam 
+//	getappstatustype
+//	getEmployees
+//	getTasks_byApplication_3
+//	getTaskStatusType
+//	getTaskTypes_applicationtab
+//	getApplicantsOnly
+
+//onMainTab
+//	getAppType,
+//	getApplications
+//	getTeam 
+//	getappstatustype
+//	getEmployees
+//	getTasks_byApplication_3
+//	getTaskStatusType
+//	getTaskTypes_applicationtab
+//	getApplicantsOnly
+//	getMyTasks
+//	getApplications_byApplicant
+//	Rept_COUNT_openTask_TeamAppTyp
+//	Rept_COUNT_unassignedTasks
+//	interactionLog_getEntityList
+//	getEntityType
+//	getApplicantStatus
+	
+//onAppSubTab
+//	interactionLog_getCallLog
+//	getAppScores_overall
+//	getAppScores_ManualOverride
+//	getAppScores
+//	getManualScores
+//	getAppType
+//	getApplications
+//	getTeam 
+//	getappstatustype
+//	getEmployees
+//	getTasks_byApplication_3
+//	getTaskStatusType
+//	getTaskTypes_applicationtab
+//	getApplicantsOnly
+
+//onScoringSubTab
+//	getApplications
+//	getAppScores_overall
+//	getAppScores_ManualOverride
+//	getAppScores
+//	getManualScores
+//	getManualOverrideTable
+//	getAwardStatusTypes
+	
+//onSettingsSubTab
+//	getAppType
+//	getAppStatusTypes
+//	getEntityType
+//	getApplicantStatus
+//	getTaskTypes_Settings
+//	getTeam
+//	getTaskStatusType
+//	getEmployees
