c933f40bcb740613df35527fa57c3c7599b9587c
dluongo <dluongo@ctcnet.us>
2024-10-07 18:56:13 +0000

    Fixed JSObject function

--- pages/State Tracker/jsobjects/mainTabQueries/mainTabQueries.js
+++ pages/State Tracker/jsobjects/mainTabQueries/mainTabQueries.js
@@ -12,7 +12,7 @@ export default {
       await getTeam.run();
       
       // Run getAppstatusType
-      await getappstatustype.run();
+      await getAppStatusTypes.run();
       
       // Run getEmployees
       await getEmployees.run();
