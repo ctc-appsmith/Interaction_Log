6cc08765c6c4513ca49aaf6c48b93f359068e135
dluongo <dluongo@ctcnet.us>
2024-10-17 21:12:39 +0000

    Updated JSO for running report queries

--- pages/State Tracker/jsobjects/mainTabQueries/mainTabQueries.js
+++ pages/State Tracker/jsobjects/mainTabQueries/mainTabQueries.js
@@ -52,6 +52,20 @@ export default {
 			
 			// Run getApplicantsOnly
 			await getApplicantsOnly.run();
+			      // Run Rept_COUNT_pendingTasks
+      await Rept_COUNT_pendingTasks.run();
+      
+      // Run Rept_COUNT_taskStatus_Score
+      await Rept_COUNT_taskStatus_Score.run();
+      
+      // Run Rept_COUNT_Applications
+      await Rept_COUNT_Applications.run();
+      
+      // Run dash_TaskbyStatus
+      await dash_TaskbyStatus.run();
+      
+      // Run Rept_getTaskStatusByTeam
+      await Rept_getTaskStatusByTeam.run();
 
       
     } catch (error) {
