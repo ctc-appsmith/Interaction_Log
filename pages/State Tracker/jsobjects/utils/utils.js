25807ea9d88d21d07948e54c3464e8217586fb75
dluongo <dluongo@ctcnet.us>
2024-11-20 22:26:52 +0000

    Created function to check user session/login credentials. Will force logout if user session has expired.

--- pages/State Tracker/jsobjects/utils/utils.js
+++ pages/State Tracker/jsobjects/utils/utils.js
@@ -7,5 +7,17 @@ isCurrentPage: (page) => {
 setCurrentPage: (page) => {
 	storeValue('page' , page);
 	navigateTo(page)
-}	
+},
+async checkSession() {
+    // Check if user data exists
+    if (get_user.data.code === 403) {
+      // Sign out from Supabase
+      await logout.logout();
+      showAlert('Your session has expired. Please log in again.', 'warning');
+      // If you have a login page, navigate to it
+      navigateTo('Login Page', {}); // Replace 'Login' with your login page name
+      return false;
+    }
+    return true;
+  }	
 }
