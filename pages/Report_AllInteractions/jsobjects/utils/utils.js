560ac0c0a7f3eb89793db323b34d19e6f25e01e7
dluongo <dluongo@ctcnet.us>
2024-11-22 14:45:28 +0000

    Updated JSObject utils w/sessionCheck and sessionCheckInterval functions to run on page load and then hourly for as long as the browser window is open. Changed the name of the user guide page for the interaction log.

--- pages/Report_AllInteractions/jsobjects/utils/utils.js
+++ pages/Report_AllInteractions/jsobjects/utils/utils.js
@@ -9,7 +9,8 @@ setCurrentPage: (page) => {
 	navigateTo(page)
 },
 async checkSession() {
-    // Check if user data exists
+    console.log("Session check executed at:", new Date().toLocaleTimeString());
+	// Check if user data exists
     if (get_user.data === null || get_user.data.code === 403) {
       // Sign out from Supabase
       await logout.logout();
@@ -19,5 +20,9 @@ async checkSession() {
       return false;
     }
     return true;
-  }		
+  },
+async sessionCheckInterval() {
+	await this.checkSession();
+	setInterval(() => this.checkSession(), 3600000);
+}	
 }
